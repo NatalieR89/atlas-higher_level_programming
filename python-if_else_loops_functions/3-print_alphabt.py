43045f4868567b623c36eaabd305d55d5c502535
NatalieR89 <natalieritch89@gmail.com>
2024-09-01 17:14:26 -0500

    Eureka!!!!

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join([chr(i) for i in range(97, 123) if i not in (101, 113)]), end='')
+for i in range(97, 123):
+    if (i != 101 and i != 113):
+        print('{:c}'.format(i), end='')
+        

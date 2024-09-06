08eaede1fd31745295dea43c5120a4d653519337
NatalieR89 <natalieritch89@gmail.com>
2024-09-06 12:35:37 -0500

    lets try this

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,6 +14,18 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+        @property
+        def size(self):
+            return self.__size
+        
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError(" size must be an interger")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
     def area(self):
 
         '''area class'''

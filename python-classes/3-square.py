9b050e38f7f2840351ef1a7f6b62d7d1c07a227a
NatalieR89 <natalieritch89@gmail.com>
2024-09-06 11:26:30 -0500

    added to init

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
 
     '''class'''
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:

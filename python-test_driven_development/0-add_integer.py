d9ac7d29007f5958d2ea5d06e4362822931b17ec
NatalieR89 <natalieritch89@gmail.com>
2024-09-08 10:54:36 -0500

    added doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+'''Module'''
+
 def add_integer(a, b=98):
+
+    '''function'''
+    
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     

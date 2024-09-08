319f905cd74801c5dc8f763e79aba17c47a76c92
NatalieR89 <natalieritch89@gmail.com>
2024-09-08 11:21:18 -0500

    typo fix L-13

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,12 +5,12 @@
 def add_integer(a, b=98):
 
     '''function'''
-    
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     
     if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer or b must be an integer")
     
     a = int(a)
     b = int(b)

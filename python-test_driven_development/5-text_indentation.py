e6fe0be29b5def430cbe707bc1baaa5984b4a9eb
NatalieR89 <natalieritch89@gmail.com>
2024-09-09 18:20:31 -0500

    code revert

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,4 +16,4 @@ def text_indentation(text):
         if char in ".?:":
             result += "\n\n"
     
-    print(".join(line.strip() for line in result.split")
+    print("\n".join(line.strip() for line in result.split("\n")))

ca653d7414cdf957db9af4da05a7efd4302c8ad4
NatalieR89 <natalieritch89@gmail.com>
2024-09-15 11:36:09 -0500

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''module'''
+
+class BaseGeometry:
+
+    '''class'''
+
+    def area(self):
+
+        '''function'''
+
+        raise Exception("area() is not implemented")

c8378b7ec0542913db6311d43cd5949bd53748bf
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 16:03:05 -0500

    PEP8 test

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+module
+'''
+def write_file(filename="", text=""):
+    '''
+    function
+    '''
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)

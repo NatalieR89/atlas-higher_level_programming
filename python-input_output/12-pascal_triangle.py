316435ee1166841282583dcc3e3f08dc8de791b3
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 18:16:25 -0500

    pycode is a meanie

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return[]
     
-triangle = [[1]]
-for a in range(1, n):
-    row = [1]
+    triangle = [[1]]
+    for a in range(1, n):
+     row = [1]
     for b in range(1, a):
-        row.append(triangle[a-1][b-1] + triangle[a-1][b])
+            row.append(triangle[a-1][b-1] + triangle[a-1][b])
     row.append(1)
     triangle.append(row)
 
-return triangle
+    return triangle

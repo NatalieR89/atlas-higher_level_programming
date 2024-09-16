46f0c2c015f5e329e567895a5ed15316b9435a6a
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 15:47:22 -0500

    working on PEP8 compliance

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+module
+'''
+def read_file(filename=""):
+    '''
+    function
+    '''
+
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())

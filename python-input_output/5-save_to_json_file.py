645fb46011d9ff1172faf60e61947b8c81c0b4a3
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 17:10:41 -0500

    a bit confused

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module'''
+
+
+import json
+
+
+def save_to_file(my_obj, filename):
+    '''function'''
+    with open(filename, "w" encoding="utf-8") as file:
+        json.dump(my_obj, file)

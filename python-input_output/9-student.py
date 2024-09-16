b2b45f42b222c6250289d5451e6677a63f7c81b4
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 17:47:02 -0500

    added student class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,11 +2,14 @@
 '''module'''
 
 
-def __init__(self, first_name, last_name, age):
-    '''function'''
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+class Student:
+
+
+    def __init__(self, first_name, last_name, age):
+        '''function'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
 def to_json(self):

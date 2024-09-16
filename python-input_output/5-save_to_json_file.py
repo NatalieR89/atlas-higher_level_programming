55ff17397ef24cc147e1293d97afd2cf39dd5482
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 17:28:50 -0500

    function name fix

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@
 import json
 
 
-def save_to_file(my_obj, filename):
+def save_to_json_file(my_obj, filename):
     '''function'''
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)

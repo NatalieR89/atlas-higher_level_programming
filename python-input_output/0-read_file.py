3b0a705a815e824cd460814fd04459ef7b65efa6
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 16:08:52 -0500

    PEP8 fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,11 +2,10 @@
 '''
 module
 '''
-def read_file(filename=""):
-    '''
-    function
-    '''
 
+
+def read_file(filename=""):
+    '''function'''
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")

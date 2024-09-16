c95ddad0b1ba7d889a490256e23a83ccf3843f5b
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 11:15:12 -0500

    2 files changed

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
 
         '''function'''
-        
+
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:

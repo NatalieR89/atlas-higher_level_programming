2b8b1df39f1ec42d296939b75d8243ce8a8d3b6a
NatalieR89 <natalieritch89@gmail.com>
2024-09-16 17:55:25 -0500

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''module'''
+
+
+class Student:
+    '''class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''function'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''function'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            return{key: value for key, value in self.__dict__.items() if key in attrs}

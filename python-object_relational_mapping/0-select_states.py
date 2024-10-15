fcaccaec4deb7c783a8f81e3ff7046e3df70881c
NatalieR89 <natalieritch89@gmail.com>
2024-10-15 18:09:20 -0500

    Test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,17 @@
 """
 Module
 """
-
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    for data in cursor.fetchall():
-        print(data)
-    cursor.close()
-    db.close()
+    """ code for script """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))

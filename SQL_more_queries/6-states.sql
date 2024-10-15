d55690d2aa852c99f5bf11489129dbcf4bd9e280
NatalieR89 <natalieritch89@gmail.com>
2024-10-15 20:37:09 +0000

    6SQL

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates hbtn usa database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);

6526aa7abeff2265c56645e77d05549c9e4460c4
NatalieR89 <natalieritch89@gmail.com>
2024-10-15 20:15:03 +0000

    1SQL

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Creates user on the server
+
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

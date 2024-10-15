2528f014784fe733f8e5cdc73f60890ada317817
NatalieR89 <natalieritch89@gmail.com>
2024-10-15 18:42:24 -0500

    ugh

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""Module
 """
+Module
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
-    Attritbute
+    Access to the database and get the states
+    from the database.
     """
-    # Capture arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=db_name)
-
-    # Create a cursor object to interact with the database
     cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-    # Execute the query to list all states sorted by id in ascending order
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows from the executed query
-    states = cur.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-
-    # Close cursor and database connection
-    cur.close()
-    db.close()
+    for row in rows:
+        print(row)

1a2ef2b8d1b8550e9bd8151ea9208f6197ab278e
NatalieR89 <natalieritch89@gmail.com>
2024-11-18 14:32:43 -0600

    10-

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+
+function factorial(n) {
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+  
+  const args = process.argv.slice(2);
+  const num = parseInt(args[0], 10);
+  
+  console.log(factorial(isNaN(num) ? 1 : num));
+  

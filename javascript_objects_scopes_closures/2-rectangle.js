da20cddb61eaaa18cc840722d62d71c3318f3296
NatalieR89 <natalieritch89@gmail.com>
2024-11-19 18:12:13 -0600

    2-1

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,11 @@
 
 class Rectangle {
     constructor(w, h) {
-      if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      if (w > 0 && h > 0)
         this.width = w;
         this.height = h;
-      } else {
-        // Return an empty object if conditions are not met
-        return {};
       }
     }
-  }
   
   module.exports = Rectangle;
   
